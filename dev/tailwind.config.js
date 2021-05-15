21a652334a060a56fb3369923eeb3c5a7a2bfbe2
Justin J Daniel <Justinjdaniel555@gmail.com>
2021-05-15 20:14:55 +0530

    :wrench: tailwindCss config updated

--- dev/tailwind.config.js
+++ dev/tailwind.config.js
@@ -1,63 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
-  // mode: 'jit', //enable just-in-time mode
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // screens: {
-    //   sm: '640px',
-    //   md: '768px',
-    //   lg: '1024px',
-    //   xl: '1280px',
-    //   '2xl': '1536px',
-    // },
-    // colors: {
-    //   gray: colors.coolGray,
-    //   blue: colors.lightBlue,
-    //   red: colors.rose,
-    //   pink: colors.fuchsia,
-    // },
     fontFamily: {
-      sans: ['Poppins','Inter var', ...defaultTheme.fontFamily.sans],
-      serif: [
-        'Merriweather',
-        'Georgia',
-        'Cambria',
-        'Times New Roman',
-        'Times',
-        'serif',
-      ],
-      mono: [
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        'Liberation Mono',
-        'Courier New',
-        'monospace',
-      ],
+      poppins: ['Poppins', 'sans-serif'],
     },
-    extend: {
-      spacing: {
-        128: '32rem',
-        144: '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-    },
+    extend: {},
   },
   variants: {
-    extend: {
-      borderColor: ['focus-visible'],
-      opacity: ['disabled'],
-    },
+    extend: {},
   },
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/aspect-ratio'),
-    // require('@tailwindcss/typography'),
-    // require('tailwindcss-children'),
-    // require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [],
 };
