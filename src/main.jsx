45fca2d15e059fead830fb15c3b0411906cd4155
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2022-09-25 15:59:01 +0530

    other Font weights added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import '@fontsource/inter';
-import '@fontsource/poppins';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
