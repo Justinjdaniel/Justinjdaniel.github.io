7d167c9810f190998d9aad8929e40bb83675662b
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2022-10-04 22:58:03 +0530

    new assets added

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import parrot from './jpg/parrot.jpg';
+import sparrow from './jpg/sparrow.jpg';
+import lab from './webp/lab.webp';
+import tech from './webp/tech.webp';
+import travel from './webp/travel.webp';
+import uiUx from './webp/ui-ux.webp';
+
+export { parrot, sparrow, lab, tech, travel, uiUx };
