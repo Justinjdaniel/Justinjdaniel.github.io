be93aa89962587283ebc15252a0fd0cef51eb27d
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2022-10-04 23:01:13 +0530

    search removed from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import { Container, Flex, Heading } from '@chakra-ui/react';
 import React from 'react';
-import SearchBar from '../components/SearchBar';
 
 const Home = () => {
   return (
     <Container maxW='1000px' px={{ base: '40px', md: '80px' }} py='25px'>
       <Flex justifyContent='space-between' mb='20px' gap='4'>
         <Heading as='h1'>Home Page</Heading>
-        <SearchBar />
       </Flex>
     </Container>
   );
