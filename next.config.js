7fa14ed0adb056698f69edccdd709eb24cdaf8cb
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-05-29 06:36:18 +0530

    chore(build): add option for building static files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	// Enable static export mode
+	output: "export",
+	// Change the output directory to 'dist'
+	distDir: "dist",
+	// Add a trailing slash to the URLs
+	trailingSlash: true,
+};
 
 module.exports = nextConfig;
