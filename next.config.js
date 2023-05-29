7554704f63eb3ac1402ea32354feede8c7ae09ea
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-05-29 07:08:02 +0530

    chore(badge): continuous deployment badge added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-	output: "export",
-	trailingSlash: true,
-	distDir: 'dist',
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 module.exports = nextConfig;
